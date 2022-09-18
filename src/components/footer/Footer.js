5695bf8f6443c048e650e6b176891e6c16817f6e
Sunmughan Swamy <sunmughaN@Gmail.com>
2022-09-18 19:58:39 +0530

    Update Footer.js

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -10,12 +10,12 @@ export default function Footer() {
     <Fade bottom duration={1000} distance="5px">
       <div className="footer-div">
         <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
-          {emoji("Made with ❤️ by Sunmughan Swamy")}
+          {emoji("Made with ❤️ by DeveloperFolio Team")}
         </p>
         <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
           Theme by{" "}
-          <a href="https://codeair.in">
-            CodeAir
+          <a href="https://github.com/saadpasta/developerFolio">
+            developerFolio
           </a>
         </p>
       </div>
