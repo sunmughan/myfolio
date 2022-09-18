cf8f06cffe922df145de41f389a93ea67e6a5552
Sunmughan Swamy <sunmughan@gmail.com>
2022-09-18 12:27:46 +0530

    My Portfolio

--- src/portfolio.js
+++ src/portfolio.js
@@ -20,26 +20,26 @@ const illustration = {
 };
 
 const greeting = {
-  username: "Saad Pasta",
-  title: "Hi all, I'm Saad",
+  username: "Sunmughan Swamy",
+  title: "Hi all, I'm Sunmughan",
   subTitle: emoji(
-    "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
+    "The Founder and CEO of CodeAir (Bespoke Software Development Company). Professionally experienced as an Indian Artist, Entrepreneur and Software Developer (Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks) mainly but touches everything, music, servers, bots, community management, events, social networks and more."
   ),
   resumeLink:
-    "https://drive.google.com/file/d/1ofFdKF_mqscH8WvXkSObnVvC9kK7Ldlu/view?usp=sharing", // Set to empty to hide the button
+    "https://drive.google.com/file/d/1EbRnkIX9o9W34GunqHkS_BuNmv3GS2Ma/view?usp=sharing", // Set to empty to hide the button
   displayGreeting: true // Set false to hide this section, defaults to true
 };
 
 // Social Media Links
 
 const socialMediaLinks = {
-  github: "https://github.com/saadpasta",
-  linkedin: "https://www.linkedin.com/in/saadpasta/",
-  gmail: "saadpasta70@gmail.com",
-  gitlab: "https://gitlab.com/saadpasta",
-  facebook: "https://www.facebook.com/saad.pasta7",
-  medium: "https://medium.com/@saadpasta",
-  stackoverflow: "https://stackoverflow.com/users/10422806/saad-pasta",
+  github: "https://github.com/sunmughan",
+  linkedin: "https://www.linkedin.com/in/sunmughan/",
+  gmail: "sunmughan@gmail.com",
+  gitlab: "https://gitlab.com/sunmughan",
+  facebook: "https://www.facebook.com/sunmughan",
+  medium: "https://medium.com/@sunmughan",
+  stackoverflow: "https://www.quora.com/profile/Sunmughan-Swamy",
   // Instagram, Twitter and Kaggle are also supported in the links!
   // To customize icons and social links, tweak src/components/SocialMedia
   display: true // Set true to display this section, defaults to false
@@ -65,12 +65,12 @@ https://fontawesome.com/icons?d=gallery */
 
   softwareSkills: [
     {
-      skillName: "html-5",
-      fontAwesomeClassname: "fab fa-html5"
+      skillName: "laravel",
+      fontAwesomeClassname: "fab fa-laravel"
     },
     {
-      skillName: "css3",
-      fontAwesomeClassname: "fab fa-css3-alt"
+      skillName: "java",
+      fontAwesomeClassname: "fab fa-java"
     },
     {
       skillName: "sass",
@@ -126,23 +126,25 @@ const educationInfo = {
   display: true, // Set false to hide this section, defaults to true
   schools: [
     {
-      schoolName: "Harvard University",
-      logo: require("./assets/images/harvardLogo.png"),
-      subHeader: "Master of Science in Computer Science",
-      duration: "September 2017 - April 2019",
-      desc: "Participated in the research of XXX and published 3 papers.",
+      schoolName: "Durg University",
+      logo: require("./assets/images/du.png"),
+      subHeader: "Bachelor in Computer Applications",
+      duration: "July 2015 - June 2018",
+      desc: "Persuaded my BCA Graduation in Durg University.",
       descBullets: [
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
+        "Scored 72% in 1st year",
+        "Scored 70.7% in 2nd year",
+        "Scored 71% in the final year"
       ]
     },
     {
-      schoolName: "Stanford University",
-      logo: require("./assets/images/stanfordLogo.png"),
-      subHeader: "Bachelor of Science in Computer Science",
-      duration: "September 2013 - April 2017",
-      desc: "Ranked top 10% in the program. Took courses about Software Engineering, Web Security, Operating Systems, ...",
-      descBullets: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit"]
+      schoolName: "DAV Senior Secondary School",
+      logo: require("./assets/images/dav.png"),
+      subHeader: "Higher Secondary",
+      duration: "June 2012 - July 2013",
+      desc: "Persuaded and completed my +2 from here. Amazing school with great learning facilities.",
+      descBullets: [
+        "Learnt discipline and ethics from here"]
     }
   ]
 };
@@ -165,7 +167,7 @@ const techStack = {
       progressPercentage: "60%"
     }
   ],
-  displayCodersrank: false // Set true to display codersrank badges section need to changes your username in src/containers/skillProgress/skillProgress.js:17:62, defaults to false
+  displayCodersrank: true // Set true to display codersrank badges section need to changes your username in src/containers/skillProgress/skillProgress.js:17:62, defaults to false
 };
 
 // Work experience section
@@ -173,30 +175,41 @@ const techStack = {
 const workExperiences = {
   display: true, //Set it to true to show workExperiences Section
   experience: [
-    {
-      role: "Software Engineer",
-      company: "Facebook",
-      companylogo: require("./assets/images/facebookLogo.png"),
-      date: "June 2018 – Present",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      {
+      role: "CTO, Lead Full Stack Developer & Project Head",
+      company: "QwikList",
+      companylogo: require("./assets/images/codeair.png"),
+      date: "July 2018 – Present",
+      desc: "Codeair is my first startup which aim to provide services on A.I, Blockchain, Cryptocurrency, Mobile Application Development, Web Development, Backend Maintenance, DevOps, Framework Development, Enterprise Solutions and Student's Academic Projects. We aim to provide quality service at best affordable price.",
       descBullets: [
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
+        "Made it to enters the top 100 growing companies in India 2021",
+        "Clients over 17 countries"
       ]
     },
     {
-      role: "Front-End Developer",
-      company: "Quora",
-      companylogo: require("./assets/images/quoraLogo.png"),
-      date: "May 2017 – May 2018",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+      role: "Chief Technology Officer",
+      company: "QwikList",
+      companylogo: require("./assets/images/qwiklist.png"),
+      date: "September 2021 – January 2022",
+      desc: "To carry on a business of rendering local search engine optimization services in India for providing business information about the characteristics, interest and other attributes of various types of businesses, projects, individuals, organizations. To carry on business in India as service provider in relation to and for facilitation of purchase and sale of any kinds of goods or services by any third parties through any medium, including by developing and operating a portal for listing of products and services and enabling users to purchase such goods or services from third party vendors and, providing any services in relation thereto, including promotion of products or services, payment collection, packaging and delivery of products, customer care service and quality control. To carry on the business of on line shopping, Internet advertising and marketing, creating virtual malls, stores, shops, creating shopping catalogues, providing secured payment processing, net commerce solutions for business to business and business to consumers. To promote, advise, organize and to carry on marketing and distribution channel for various goods and service providers and to provide various solution for digital payments for its users through multiple platforms such as website, mobile apps and through third party applications.",
+      descBullets: [
+        "Developed the digital platform for Qwiklist (Web application and Mobile App).",
+        "Registered the company in Private Limited and helped in getting recognised in Indore (M.P)."
+      ]
     },
     {
-      role: "Software Engineer Intern",
-      company: "Airbnb",
-      companylogo: require("./assets/images/airbnbLogo.png"),
-      date: "Jan 2015 – Sep 2015",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+      role: "Lead Software Developer",
+      company: "National Informatics Centre",
+      companylogo: require("./assets/images/nic.jpeg"),
+      date: "Dec 2019 – May 2020",
+      desc: "I've been worked here as a lead software engineer. Understanding the basic needs and requirements of the company i along with my team used to share the desired output in the form of softwares to run the industry among its competitors freely."
+    },
+    {
+      role: "Tech Blogger",
+      company: "DroidAdda",
+      companylogo: require("./assets/images/dadda.png"),
+      date: "Jul 2015 – Dec 2017",
+      desc: "I created Droidadda on July 29 2015 as a personal blog. Soon after getting huge response from the user i made it a public blogging platform where i post latest trending articles on various topics like Android,Blogging,Games,Apps,Development,Marketing,etc for my beloved readers on Droidadda."
     }
   ]
 };
@@ -216,25 +229,26 @@ const bigProjects = {
   subtitle: "SOME STARTUPS AND COMPANIES THAT I HELPED TO CREATE THEIR TECH",
   projects: [
     {
-      image: require("./assets/images/saayaHealthLogo.webp"),
-      projectName: "Saayahealth",
-      projectDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+      image: require("./assets/images/amd.png"),
+      projectName: "ActionalMD",
+      projectDesc: "A healthcare startup aims to deliver the best medical practices to people and improve modernization in digital healthcare industry. I along with my team in CodeAir, were designed and frontend and Backend of the initial phase of the ActionalMD back in 2021 at its earliest release.",
       footerLink: [
         {
           name: "Visit Website",
-          url: "http://saayahealth.com/"
+          url: "https://actionalmd.com/"
         }
         //  you can add extra buttons here.
       ]
     },
     {
-      image: require("./assets/images/nextuLogo.webp"),
-      projectName: "Nextu",
-      projectDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+      image: require("./assets/images/taxdraw.png"),
+      projectName: "TaxDraw",
+      projectDesc: "A legal business company which helps businesses to registered officially as a legal entity in India
+ I've improved the backend in Laravel as an outsourcing partner to them.",
       footerLink: [
         {
           name: "Visit Website",
-          url: "http://nextu.se/"
+          url: "http://taxdraw.com/"
         }
       ]
     }
@@ -249,54 +263,62 @@ const achievementSection = {
   title: emoji("Achievements And Certifications 🏆 "),
   subtitle:
     "Achievements, Certifications, Award Letters and Some Cool Stuff that I have done !",
-
+    
   achievementsCards: [
     {
       title: "Google Code-In Finalist",
       subtitle:
-        "First Pakistani to be selected as Google Code-in Finalist from 4000 students from 77 different countries.",
-      image: require("./assets/images/codeInLogo.webp"),
-      imageAlt: "Google Code-In Logo",
+        "My first certification and achievement comes from here, I've earned the Badge and certificate by scoring good among 300K other programmers.",
+      image: require("./assets/images/gpca.png"),
+      imageAlt: "Google Certification Badge",
       footerLink: [
         {
           name: "Certification",
-          url: "https://drive.google.com/file/d/0B7kazrtMwm5dYkVvNjdNWjNybWJrbndFSHpNY2NFV1p4YmU0/view?usp=sharing"
+          url: "https://drive.google.com/file/d/1T6qYVOWA2OvY0m_eZXh_gofOkP2dYGr7/view"
         },
         {
-          name: "Award Letter",
-          url: "https://drive.google.com/file/d/0B7kazrtMwm5dekxBTW5hQkg2WXUyR3QzQmR0VERiLXlGRVdF/view?usp=sharing"
-        },
+          name: "Date of Achievement",
+          subtitle: "Apr 2018"
+        }        
+      ]
+    },    
+    {
+      title: "Google Professional Cloud Architect",
+      subtitle:
+        "Developed a Google Cloud PWA that is available on 2 Billion devices world wide.",
+      image: require("./assets/images/gcpa.png"),
+      imageAlt: "Google Professional Cloud Architect",
+      footerLink: [
         {
-          name: "Google Code-in Blog",
-          url: "https://opensource.googleblog.com/2019/01/google-code-in-2018-winners.html"
+          name: "Date of Achievement",
+          subtitle: "Jul 2019"
         }
       ]
     },
     {
-      title: "Google Assistant Action",
+      title: "IBM Certified Solution Developer",
       subtitle:
-        "Developed a Google Assistant Action JavaScript Guru that is available on 2 Billion devices world wide.",
-      image: require("./assets/images/googleAssistantLogo.webp"),
-      imageAlt: "Google Assistant Action Logo",
+        "Developed a Google Cloud PWA that is available on 2 Billion devices world wide.",
+      image: require("./assets/images/gcpa.png"),
+      imageAlt: "Google Professional Cloud Architect",
       footerLink: [
         {
-          name: "View Google Assistant Action",
-          url: "https://assistant.google.com/services/a/uid/000000100ee688ee?hl=en"
+          name: "Date of Achievement",
+          subtitle: "Oct 2016"
         }
       ]
     },
 
     {
-      title: "PWA Web App Developer",
-      subtitle: "Completed Certifcation from SMIT for PWA Web App Development",
-      image: require("./assets/images/pwaLogo.webp"),
-      imageAlt: "PWA Logo",
+      title: "AWS Certified Associate Developer",
+      subtitle: "Completed Certifcation from AWS in Web App Development",
+      image: require("./assets/images/awscda.png"),
+      imageAlt: "AWS Certification",
       footerLink: [
-        {name: "Certification", url: ""},
         {
-          name: "Final Project",
-          url: "https://pakistan-olx-1.firebaseapp.com/"
-        }
+        	name: " Date of Achievement", 
+        	subtitle: "Sep 2020"
+        	}
       ]
     }
   ],
@@ -312,16 +334,16 @@ const blogSection = {
   displayMediumBlogs: "true", // Set true to display fetched medium blogs instead of hardcoded ones
   blogs: [
     {
-      url: "https://blog.usejournal.com/create-a-google-assistant-action-and-win-a-google-t-shirt-and-cloud-credits-4a8d86d76eae",
-      title: "Win a Google Assistant Tshirt and $200 in Google Cloud Credits",
+      url: "https://medium.com/@sunmughan/vidhhya-complete-online-school-management-solution-5c543a6127ab",
+      title: "Vidhhya — Complete Online School Management Solution by CodeAir",
       description:
-        "Do you want to win $200 and Google Assistant Tshirt by creating a Google Assistant Action in less then 30 min?"
+        "Codeair has developed a unified solution for online school and learning. Vidhhya School Management ERP (Website + Admin Panel + Android/iOS Mobile Application) aims at easing administrative hassles and enhancing efficiency, and productivity. The Solution is cloud based and supports various modern technologies such as – Online payment gateway, RFID smart cards, biometric, mobile apps, email alerts, etc. School management software is agile and can be tailored according to the requirements of the institution."
     },
     {
-      url: "https://medium.com/@saadpasta/why-react-is-the-best-5a97563f423e",
-      title: "Why REACT is The Best?",
+      url: "https://medium.com/@sunmughan/iconfy-instant-free-use-video-conferencing-app-for-android-7599429a97fe",
+      title: "iConfy — Instant Free use video conferencing app for android 📸",
       description:
-        "React is a JavaScript library for building User Interface. It is maintained by Facebook and a community of individual developers and companies."
+        "iConfy is a free video conferencing and video meeting app that allows you to communicate with your friends, family or colleagues with ease."
     }
   ],
   display: true // Set false to hide this section, defaults to true
@@ -337,10 +359,10 @@ const talkSection = {
 
   talks: [
     {
-      title: "Build Actions For Google Assistant",
-      subtitle: "Codelab at GDG DevFest Karachi 2019",
-      slides_url: "https://bit.ly/saadpasta-slides",
-      event_url: "https://www.facebook.com/events/2339906106275053/"
+      title: "PROJECT PRESENTATION",
+      subtitle: "Detailed slides of Vidhhya School Management System and Students Academic Project Development",
+      slides_url: "https://drive.google.com/file/d/12zpqbCjplwHU3dgY-YfyRM1br8bY_GnP/view?usp=drivesdk",
+      event_url: "https://drive.google.com/file/d/1C9zwAUKvKCfGuLLObXiJC1h1Hicri5og/view?usp=drivesdk"
     }
   ],
   display: true // Set false to hide this section, defaults to true
@@ -350,31 +372,31 @@ const talkSection = {
 
 const podcastSection = {
   title: emoji("Podcast 🎙️"),
-  subtitle: "I LOVE TO TALK ABOUT MYSELF AND TECHNOLOGY",
+  subtitle: "I LOVE TO TALK ABOUT PROGRAMMING AND TECHNOLOGY 🥸",
 
   // Please Provide with Your Podcast embeded Link
   podcast: [
-    "https://anchor.fm/codevcast/embed/episodes/DevStory---Saad-Pasta-from-Karachi--Pakistan-e9givv/a-a15itvo"
+    ""
   ],
-  display: true // Set false to hide this section, defaults to true
+  display: false // Set false to hide this section, defaults to true
 };
 
 const contactInfo = {
   title: emoji("Contact Me ☎️"),
   subtitle:
-    "Discuss a project or just want to say hi? My Inbox is open for all.",
-  number: "+92-0000000000",
-  email_address: "saadpasta70@gmail.com"
+    "Have a project in mind and want to discuss ? Feel free to connect me over here 😌",
+  number: "+91-9584215603",
+  email_address: "sunmughan@gmail.com"
 };
 
 // Twitter Section
 
 const twitterDetails = {
-  userName: "twitter", //Replace "twitter" with your twitter username without @
+  userName: "codeair1", //Replace "twitter" with your twitter username without @
   display: true // Set true to display this section, defaults to false
 };
 
-const isHireable = false; // Set false if you are not looking for a job. Also isHireable will be display as Open for opportunities: Yes/No in the GitHub footer
+const isHireable = true; // Set false if you are not looking for a job. Also isHireable will be display as Open for opportunities: Yes/No in the GitHub footer
 
 export {
   illustration,
